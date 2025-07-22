c625ac4a3772d2a171d8f29981b0b34a23b3c45b
AntoniaJost <antonia.jost@web.de>
2025-07-22 09:16:13 +0200

    change settings for first long run

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,7 +3,8 @@
 #SBATCH --output=logs/pipeline.log
 #SBATCH --error=logs/pipeline.log
 #SBATCH --time=04:00:00
-#SBATCH --partition=standard
+#SBATCH --partition=compute
+#SBATCH --account=bk1450
 
 module load python/3.11.5
 source /work/bk1450/a270220/aimip/bin/activate
