0a476079f99120899c098b2f7faade4ed51871a2
Robert Brunstein <robert.brunstein@ovgu.de>
2025-10-12 15:35:26 +0200

    Added replacement of land points

--- evaluation/rollout.sh
+++ evaluation/rollout.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#SBATCH --job-name=AW-M-0-aimip-stats-forcings_surface-interpolgt
 #SBATCH --account=bk1450
 #SBATCH --qos=normal
 #SBATCH --partition=gpu
@@ -16,22 +17,29 @@ lenv  # activate weather env
 export HYDRA_FULL_ERROR=1
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
+# First use right branch of geoarches repository
+cdga
+git checkout feature/forcings
+
+# Switch back to geoarches_evaluation repository
+cdge 
+cd evaluation
 
 ######### ArchesWeatherDet #########
-name="AW-M-0-aimip-stats-forcings_surface"
-aimip_name="AW-M-0-aimip-stats-forcings_surface"
-mkdir -p logs/${name}/${SLURM_JOB_ID}
+name="AW-M-1-aimip-w_forcings-interpolgt"
+aimip_name="AW-M-avg-aimip-w_forcings-interpolgt"
 echo "Running on ${SLURM_JOB_NUM_NODES} nodes with ${SLURM_CPUS_PER_TASK} CPUs per task."
 echo "Using ${SLURM_GPUS_PER_NODE} GPUs."
 srun --cpu-bind=none --mem-bind=none --mem=0  --cpus-per-task=8 python3 rollout.py \
     ++model_name=${name} \
     ++aimip.aimip_name=${aimip_name} \
     '++aimip.continue_rollout=False' \
-    '++aimip.member=0' \
+    '++aimip.member="avg"' \
+    '++aimip.sst_scenario="0"' \
     '++start_timestamp="1980-01-01T12:00"' \
-    '++end_timestamp="2030-01-01T12:00"' \
-    '++replace_nans_from_state=True' \
-    > ${SLURM_SUBMIT_DIR}/logs/${name}/${SLURM_JOB_ID}/output.txt
+    '++end_timestamp="2000-12-31T12:00"' \
+    '++aimip.ablate_forcings=False' \
+    '++aimip.replace_land_grid_from_forcings=False' \
 
 ######### ArchesWeatherGen #########
 #name="archesgen-m-gc-sst_sic-weight_01"
